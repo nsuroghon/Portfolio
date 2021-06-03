7e00947fbc3cba1883a8595bf64cc2aa35ff011b
Niv Suroghon <nsuroghon@gmail.com>
2021-06-03 15:02:38 -0400

    linkedin anchor tag

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -14,9 +14,9 @@ export default class ContactUs extends Component {
           <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
-                <h4 style={{fontWeight:"bold"}}>Linked in :
+                <a><h4 style={{fontWeight:"bold"}}>Linked in :
                   {resumeData.linkedinId}
-                </h4>
+                </h4></a>
                 <a href={"mailto:" + resumeData.email}><h4 style={{fontWeight:"bold"}}>Email :
                   {resumeData.email}
                 </h4></a>
